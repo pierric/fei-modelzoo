ebff7ccf0d1cbf8360fefb2205ad0b4a46337c86
Jiasen Wu <jiasenwu@hotmail.com>
2020-06-15 23:13:32 +0200

    refactor resnext

--- src/MXNet/NN/ModelZoo/Resnext.hs
+++ src/MXNet/NN/ModelZoo/Resnext.hs
@@ -1,13 +1,10 @@
 module MXNet.NN.ModelZoo.Resnext where
 
-import RIO
-import qualified RIO.Text as T
+import           Formatting
+import           RIO
 
-import MXNet.Base
-import MXNet.NN.Layer
-
-(##) :: Text -> Text -> Text
-(##) = T.append
+import           MXNet.Base
+import           MXNet.NN.Layer
 
 -- ResNet
 -- #layer: 164
@@ -17,50 +14,49 @@ import MXNet.NN.Layer
 -- #filter of stage 2: 128
 -- #filter of stage 3: 256
 
-symbol :: DType a => IO (Symbol a)
-symbol = do
-    x  <- variable "x"
-    y  <- variable "y"
+rootName = "resnext0"
 
-    xcp <- identity "id" (#data := x .& Nil)
+symbol :: SymbolHandle -> Layer SymbolHandle
+symbol dat = unique rootName $ do
+    bnx <- named "batchnorm0" $
+           batchnorm (#data := dat
+                   .& #eps := eps
+                   .& #momentum := bn_mom
+                   .& #fix_gamma := True .& Nil)
 
-    bnx <- batchnorm "bn-x" (#data := xcp
-                          .& #eps := eps
-                          .& #momentum := bn_mom
-                          .& #fix_gamma := True .& Nil)
+    cvx <- named "conv0" $
+           convolution (#data := bnx
+                     .& #kernel := [3,3]
+                     .& #num_filter := 16
+                     .& #stride := [1,1]
+                     .& #pad := [1,1]
+                     .& #workspace := conv_workspace
+                     .& #no_bias := True .& Nil)
 
-    cvx <- convolution "conv-bn-x" (#data := bnx
-                                 .& #kernel := [3,3]
-                                 .& #num_filter := 16
-                                 .& #stride := [1,1]
-                                 .& #pad := [1,1]
-                                 .& #workspace := conv_workspace
-                                 .& #no_bias := True .& Nil)
-
-    bdy <- foldM (\layer (num_filter, stride, dim_match, name) ->
-                    residual name (#data       := layer
-                                .& #num_filter := num_filter
-                                .& #stride     := stride
-                                .& #dim_match  := dim_match .& resargs))
+    bdy <- foldM (\layer (num_filter, stride, dim_match, stage_id, unit_id) ->
+                    unique (sformat ("stage" % int) stage_id) $ residual unit_id
+                        (#data       := layer
+                      .& #num_filter := num_filter
+                      .& #stride     := stride
+                      .& #dim_match  := dim_match .& resargs))
                  cvx
                  residual'parms
 
-    pool1 <- pooling "pool1" (#data := bdy
-                           .& #kernel := [7,7]
-                           .& #pool_type := #avg
-                           .& #global_pool := True .& Nil)
-    flat  <- flatten "flat-1" (#data := pool1 .& Nil)
-    fc1   <- fullyConnected "fc-1" (#data := flat
-                                 .& #num_hidden := 10 .& Nil)
-    Symbol <$> softmaxoutput "softmax" (#data := fc1
-                                     .& #label := y .& Nil)
+    pool1 <- pooling (#data := bdy
+                   .& #kernel := [7,7]
+                   .& #pool_type := #avg
+                   .& #global_pool := True .& Nil)
+    flat <- flatten (#data := pool1 .& Nil)
+    named "dense0" $ fullyConnected (#data := flat
+                                  .& #num_hidden := 10 .& Nil)
   where
     bn_mom = 0.9 :: Float
     conv_workspace = 256 :: Int
     eps = 2e-5 :: Double
-    residual'parms =  [ (64,  [1,1], False, "stage1-unit1") ] ++ map (\i -> (64,  [1,1], True, "stage1-unit" ## tshow i)) [2..18 :: Int]
-                   ++ [ (128, [2,2], False, "stage2-unit1") ] ++ map (\i -> (128, [1,1], True, "stage2-unit" ## tshow i)) [2..18 :: Int]
-                   ++ [ (256, [2,2], False, "stage3-unit1") ] ++ map (\i -> (256, [1,1], True, "stage3-unit" ## tshow i)) [2..18 :: Int]
+    residual'parms =
+        [(64,  [1,1], False, 1::Int, 1::Int)] ++ [(64,  [1,1], True, 1, i) | i <- [2..18]]
+     ++ [(128, [2,2], False, 2, 1)] ++ [(128, [1,1], True, 2, i) | i <- [2..18]]
+     ++ [(256, [2,2], False, 3, 1)] ++ [(256, [1,1], True, 3, i) | i <- [2..18]]
     resargs = #bottle_neck := True .& #workspace := conv_workspace .& #memonger := False .& Nil
 
 type instance ParameterList "_residual_layer(resnext)" =
@@ -74,8 +70,8 @@ type instance ParameterList "_residual_layer(resnext)" =
    , '("workspace"  , 'AttrOpt Int)
    , '("memonger"   , 'AttrOpt Bool) ]
 residual :: (Fullfilled "_residual_layer(resnext)" args)
-         => Text -> ArgsHMap "_residual_layer(resnext)" args -> IO SymbolHandle
-residual name args = do
+         => Int -> ArgsHMap "_residual_layer(resnext)" args -> Layer SymbolHandle
+residual _id args = do
     let dat        = args ! #data
         num_filter = args ! #num_filter
         stride     = args ! #stride
@@ -87,105 +83,117 @@ residual name args = do
         memonger   = fromMaybe False$ args !? #memonger
         eps = 2e-5 :: Double
     if bottle_neck
-      then do
-        conv1 <- convolution (name ## "-conv1") (#data      := dat
-                                              .& #kernel    := [1,1]
-                                              .& #num_filter:= num_filter `div` 2
-                                              .& #stride    := [1,1]
-                                              .& #pad       := [0,0]
-                                              .& #workspace := workspace
-                                              .& #no_bias   := True .& Nil)
-        bn1 <- batchnorm (name ## "-bn1") (#data      := conv1
-                                        .& #eps       := eps
-                                        .& #momentum  := bn_mom
-                                        .& #fix_gamma := False .& Nil)
-        act1 <- activation (name ## "-relu1") (#data      := bn1
-                                            .& #act_type  := #relu .& Nil)
-        conv2 <- convolution (name ## "-conv2") (#data      := act1
-                                              .& #kernel    := [3,3]
-                                              .& #num_filter:= num_filter `div` 2
-                                              .& #stride    := stride
-                                              .& #pad       := [1,1]
-                                              .& #num_group := num_group
-                                              .& #workspace := workspace
-                                              .& #no_bias   := True .& Nil)
-        bn2 <- batchnorm (name ## "-bn2") (#data      := conv2
-                                        .& #eps       := eps
-                                        .& #momentum  := bn_mom
-                                        .& #fix_gamma := False .& Nil)
-        act2 <- activation (name ## "-relu2") (#data      := bn2
-                                            .& #act_type  := #relu .& Nil)
-        conv3 <- convolution (name ## "-conv3") (#data      := act2
-                                              .& #kernel    := [1,1]
-                                              .& #num_filter:= num_filter
-                                              .& #stride    := [1,1]
-                                              .& #pad       := [0,0]
-                                              .& #workspace := workspace
-                                              .& #no_bias   := True .& Nil)
-        bn3 <- batchnorm (name ## "-bn3") (#data      := conv3
-                                        .& #eps       := eps
-                                        .& #momentum  := bn_mom
-                                        .& #fix_gamma := False .& Nil)
-        shortcut <- if dim_match
-                    then return dat
-                    else do
-                        shortcut_conv <- convolution (name ## "-sc") (#data        := dat
-                                                                   .& #kernel      := [1,1]
-                                                                   .& #num_filter  := num_filter
-                                                                   .& #stride      := stride
-                                                                   .& #workspace   := workspace
-                                                                   .& #no_bias     := True .& Nil)
-                        batchnorm (name ## "-sc-bn") (#data        := shortcut_conv
-                                                   .& #eps         := eps
-                                                   .& #momentum    := bn_mom
-                                                   .& #fix_gamma   := False .& Nil)
+    then do
+        conv1 <- named (sformat ("conv" % int) _id) $
+                 convolution (#data      := dat
+                           .& #kernel    := [1,1]
+                           .& #num_filter:= num_filter `div` 2
+                           .& #stride    := [1,1]
+                           .& #pad       := [0,0]
+                           .& #workspace := workspace
+                           .& #no_bias   := True .& Nil)
+        bn1   <- named (sformat ("batchnorm" % int) _id) $
+                 batchnorm (#data      := conv1
+                         .& #eps       := eps
+                         .& #momentum  := bn_mom
+                         .& #fix_gamma := False .& Nil)
+        act1  <- activation (#data      := bn1
+                          .& #act_type  := #relu .& Nil)
+        conv2 <- named (sformat ("conv" % int) (_id + 1)) $
+                 convolution (#data      := act1
+                           .& #kernel    := [3,3]
+                           .& #num_filter:= num_filter `div` 2
+                           .& #stride    := stride
+                           .& #pad       := [1,1]
+                           .& #num_group := num_group
+                           .& #workspace := workspace
+                           .& #no_bias   := True .& Nil)
+        bn2   <- named (sformat ("batchnorm" % int) (_id + 1)) $
+                 batchnorm (#data      := conv2
+                         .& #eps       := eps
+                         .& #momentum  := bn_mom
+                         .& #fix_gamma := False .& Nil)
+        act2  <- activation (#data      := bn2
+                          .& #act_type  := #relu .& Nil)
+        conv3 <- named (sformat ("conv" % int) (_id + 2)) $
+                 convolution (#data      := act2
+                           .& #kernel    := [1,1]
+                           .& #num_filter:= num_filter
+                           .& #stride    := [1,1]
+                           .& #pad       := [0,0]
+                           .& #workspace := workspace
+                           .& #no_bias   := True .& Nil)
+        bn3   <- named (sformat ("batchnorm" % int) (_id + 2)) $
+                 batchnorm (#data      := conv3
+                         .& #eps       := eps
+                         .& #momentum  := bn_mom
+                         .& #fix_gamma := False .& Nil)
+        shortcut <-
+            if dim_match
+            then return dat
+            else do
+                shortcut_conv <- named (sformat ("conv" % int) (_id + 3)) $
+                                 convolution (#data        := dat
+                                           .& #kernel      := [1,1]
+                                           .& #num_filter  := num_filter
+                                           .& #stride      := stride
+                                           .& #workspace   := workspace
+                                           .& #no_bias     := True .& Nil)
+                named (sformat ("conv" % int) (_id + 3)) $
+                    batchnorm (#data        := shortcut_conv
+                            .& #eps         := eps
+                            .& #momentum    := bn_mom
+                            .& #fix_gamma   := False .& Nil)
         when memonger $
-          void $ mxSymbolSetAttr shortcut "mirror_stage" "true"
-        eltwise <- plus name (#lhs := bn3
-                           .& #rhs := shortcut .& Nil)
-        activation (name ## "-relu") (#data     := eltwise
-                                   .& #act_type := #relu .& Nil)
-      else do
-        conv1 <- convolution (name ## "-conv1") (#data        := dat
-                                              .& #kernel      := [3,3]
-                                              .& #num_filter  := num_filter
-                                              .& #stride      := stride
-                                              .& #pad         := [1,1]
-                                              .& #workspace   := workspace
-                                              .& #no_bias     := True .& Nil)
-        bn1 <- batchnorm (name ## "-bn1") (#data        := conv1
-                                        .& #eps         := eps
-                                        .& #momentum    := bn_mom
-                                        .& #fix_gamma   := False .& Nil)
-        act1 <- activation (name ## "-relu1") (#data        := bn1
-                                            .& #act_type    := #relu .& Nil)
-        conv2 <- convolution (name ## "-conv2") (#data        := act1
-                                              .& #kernel      := [3,3]
-                                              .& #num_filter  := num_filter
-                                              .& #stride      := [1,1]
-                                              .& #pad         := [1,1]
-                                              .& #workspace   := workspace
-                                              .& #no_bias     := True .& Nil)
-        bn2 <- batchnorm (name ## "-bn2") (#data        := conv2
-                                        .& #eps         := eps
-                                        .& #momentum    := bn_mom
-                                        .& #fix_gamma   := False .& Nil)
-        shortcut <- if dim_match
-                    then return dat
-                    else do
-                        shortcut_conv <- convolution (name ## "-sc") (#data        := act1
-                                                                   .& #kernel      := [1,1]
-                                                                   .& #num_filter  := num_filter
-                                                                   .& #stride      := stride
-                                                                   .& #workspace   := workspace
-                                                                   .& #no_bias     := True .& Nil)
-                        batchnorm (name ## "-sc-bn") (#data        := shortcut_conv
-                                                   .& #eps         := eps
-                                                   .& #momentum    := bn_mom
-                                                   .& #fix_gamma   := False .& Nil)
+          liftIO $ void $ mxSymbolSetAttr shortcut "mirror_stage" "true"
+        eltwise <- plus (#lhs := bn3 .& #rhs := shortcut .& Nil)
+        activation (#data := eltwise .& #act_type := #relu .& Nil)
+    else do
+        conv1 <- named (sformat ("conv" % int) _id) $
+                 convolution (#data        := dat
+                           .& #kernel      := [3,3]
+                           .& #num_filter  := num_filter
+                           .& #stride      := stride
+                           .& #pad         := [1,1]
+                           .& #workspace   := workspace
+                           .& #no_bias     := True .& Nil)
+        bn1   <- named (sformat ("batchnorm" % int) _id) $
+                 batchnorm (#data        := conv1
+                         .& #eps         := eps
+                         .& #momentum    := bn_mom
+                         .& #fix_gamma   := False .& Nil)
+        act1  <- activation (#data        := bn1
+                          .& #act_type    := #relu .& Nil)
+        conv2 <- named (sformat ("conv" % int) (_id + 1)) $
+                 convolution (#data        := act1
+                           .& #kernel      := [3,3]
+                           .& #num_filter  := num_filter
+                           .& #stride      := [1,1]
+                           .& #pad         := [1,1]
+                           .& #workspace   := workspace
+                           .& #no_bias     := True .& Nil)
+        bn2   <- named (sformat ("batchnorm" % int) (_id + 1)) $
+                 batchnorm (#data        := conv2
+                         .& #eps         := eps
+                         .& #momentum    := bn_mom
+                         .& #fix_gamma   := False .& Nil)
+        shortcut <-
+            if dim_match
+            then return dat
+            else do
+                shortcut_conv <- named (sformat ("conv" % int) (_id + 2)) $
+                                 convolution (#data        := act1
+                                           .& #kernel      := [1,1]
+                                           .& #num_filter  := num_filter
+                                           .& #stride      := stride
+                                           .& #workspace   := workspace
+                                           .& #no_bias     := True .& Nil)
+                named (sformat ("batchnorm" % int) (_id + 2)) $
+                    batchnorm (#data        := shortcut_conv
+                            .& #eps         := eps
+                            .& #momentum    := bn_mom
+                            .& #fix_gamma   := False .& Nil)
         when memonger $
-          void $ mxSymbolSetAttr shortcut "mirror_stage" "true"
-        eltwise <- plus name (#lhs := bn2
-                           .& #rhs := shortcut .& Nil)
-        activation (name ## "-relu") (#data     := eltwise
-                                   .& #act_type := #relu .& Nil)
+          liftIO $ void $ mxSymbolSetAttr shortcut "mirror_stage" "true"
+        eltwise <- plus (#lhs := bn2 .& #rhs := shortcut .& Nil)
+        activation (#data := eltwise .& #act_type := #relu .& Nil)
