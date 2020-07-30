8a33cdf5e5f1e60583328f8894a0cc08f290b39a
Jiasen Wu <jiasenwu@hotmail.com>
2020-07-30 06:27:24 +0200

    bug fix in FPN and RCNN

--- src/MXNet/NN/ModelZoo/RCNN/FPN.hs
+++ src/MXNet/NN/ModelZoo/RCNN/FPN.hs
@@ -18,49 +18,58 @@ fpnFeatureExpander sym output_layers = do
     sym <- internals sym
     layers <- mapM (at' sym) layer_names
 
-    outputs <- liftIO $ newIORef []
-    foldM_ (topDownPass outputs) Nothing (NE.zip layer_filters layers)
+    outputs <- liftIO $ newIORef (error "empty")
+    sequential "fpn" $ do
+        foldM_ (topDownPass outputs) Nothing (NE.zip layer_filters layers)
     liftIO $ readIORef outputs
 
   where
     (layer_names, layer_filters) = NE.unzip $ NE.reverse output_layers
-    topDownPass outputs Nothing (nflt, layer) = do
-        y <- convolution (#data := layer
+    topDownPass outputs Nothing (nflt, layer) = subscope_next_name $ unique' $ do
+        y <- named "conv1" $
+             convolution (#data := layer
                        .& #num_filter := nflt
                        .& #kernel := [1,1]
                        .& #pad := [0, 0]
                        .& #stride := [1,1]
                        .& #no_bias := True .& Nil)
-        y <- batchnorm   (#data := y .& Nil)
-        y <- convolution (#data := y
-                       .& #num_filter := nflt
-                       .& #kernel := [3, 3]
-                       .& #pad := [1, 1]
-                       .& #stride := [1,1]
-                       .& #no_bias := True .& Nil)
-        y <- batchnorm   (#data := y .& Nil)
-        modifyIORef outputs (y NE.<|)
+        y <- named "bn0" $
+             batchnorm   (#data := y .& Nil)
+        out <- named "conv2" $
+               convolution (#data := y
+                         .& #num_filter := nflt
+                         .& #kernel := [3, 3]
+                         .& #pad := [1, 1]
+                         .& #stride := [1,1]
+                         .& #no_bias := True .& Nil)
+        out <- named "bn1" $
+               batchnorm   (#data := out .& Nil)
+        writeIORef outputs [out]
         return (Just y)
-    topDownPass outputs (Just prev) (nflt, layer) = do
-        y <- convolution (#data := layer
+    topDownPass outputs (Just prev) (nflt, layer) = subscope_next_name $ unique' $ do
+        y <- named "conv1" $
+             convolution (#data := layer
                        .& #num_filter := nflt
                        .& #kernel := [1,1]
                        .& #pad := [0, 0]
                        .& #stride := [1,1]
                        .& #no_bias := True .& Nil)
-        y <- batchnorm   (#data := y .& Nil)
-        y <- prim _UpSampling
-                         (#data := [y]
+        y <- named "bn0" $
+             batchnorm   (#data := y .& Nil)
+        prev_up <- prim _UpSampling
+                         (#data := [prev]
                        .& #num_args := 1
                        .& #scale := 2
                        .& #sample_type := #nearest .& Nil)
-        y <- add_ prev y
-        y <- convolution (#data := y
-                       .& #num_filter := nflt
-                       .& #kernel := [3, 3]
-                       .& #pad := [1, 1]
-                       .& #stride := [1,1]
-                       .& #no_bias := True .& Nil)
-        y <- batchnorm   (#data := y .& Nil)
-        modifyIORef outputs (y NE.<|)
+        y <- add_ prev_up y
+        out <- named "conv2" $
+               convolution (#data := y
+                         .& #num_filter := nflt
+                         .& #kernel := [3, 3]
+                         .& #pad := [1, 1]
+                         .& #stride := [1,1]
+                         .& #no_bias := True .& Nil)
+        out <- named "bn1" $
+               batchnorm   (#data := out .& Nil)
+        modifyIORef outputs (out NE.<|)
         return (Just y)
--- src/MXNet/NN/ModelZoo/RCNN/RCNN.hs
+++ src/MXNet/NN/ModelZoo/RCNN/RCNN.hs
@@ -13,6 +13,14 @@ rcnnSampler :: Int -> Int -> Int -> Float -> Float -> Int
             -> Layer (SymbolHandle, SymbolHandle, SymbolHandle)
 rcnnSampler batch_size num_proposal num_sample fg_overlap fg_fraction max_num_gt
             rois scores gt_boxes = do
+    -- B: batch_size, N: num_proposal (post-topk), S: num_sample (rcnn_batch_rois)
+    -- rois:     (B,N,4), min_x, min_y, max_x, max_y
+    -- scores:   (B,N,1), value range [0,1], -1 for being ignored
+    -- gt_boxes: (B,M,4), min_x, min_y, max_x, max_y
+    -- return:
+    --   rois:   (B,S,4)
+    --   samples:(B,S), value -1 (negative), 0 (ignore), 1 (positive)
+    --   matches:(B,S), value [0, M)
     (rois, samples, matches) <- unzip3 <$> mapM sampler [0..batch_size-1]
 
     rois    <- stack (#data := rois    .& #axis := 0 .& Nil)
@@ -61,11 +69,11 @@ rcnnSampler batch_size num_proposal num_sample fg_overlap fg_fraction max_num_gt
 
           -- shuffle mask and ious_argmax
           rand <- prim S._random_uniform (#low := 0 .& #high := 1
-                                     .& #shape := [num_proposal + max_num_gt] .& Nil)
+                                       .& #shape := [num_proposal + max_num_gt] .& Nil)
           rand <- prim S.slice_like (#data := rand .& #shape_like := ious_max .& Nil)
-          index<- prim S.argsort   (#data := rand .& Nil)
-          mask <- takeI mask index
-          ious_argmax <- takeI ious_argmax index
+          index<- prim S.argsort    (#data := rand .& Nil)
+          mask <- takeI index mask
+          ious_argmax <- takeI index ious_argmax
 
           -- sort in order of pos, neg, ignore
           order <- prim S.argsort (#data := mask .& #is_ascend := False .& Nil)
@@ -75,9 +83,9 @@ rcnnSampler batch_size num_proposal num_sample fg_overlap fg_fraction max_num_gt
                                    .& #axis  := 0
                                    .& #begin := 0
                                    .& #end := Just max_pos .& Nil)
-          topk_indices <- takeI index       topk
-          topk_samples <- takeI mask        topk
-          topk_matches <- takeI ious_argmax topk
+          topk_indices <- takeI topk index
+          topk_samples <- takeI topk mask
+          topk_matches <- takeI topk ious_argmax
 
           -- sample the positive class
           pos_class <- onesLike topk_samples
@@ -93,38 +101,39 @@ rcnnSampler batch_size num_proposal num_sample fg_overlap fg_fraction max_num_gt
           -- sample the negative class
           index       <- prim S.slice_axis (#data := index
                                          .& #axis  := 0
-                                         .& #begin := 0
-                                         .& #end := Just max_pos .& Nil)
+                                         .& #begin := max_pos
+                                         .& #end := Nothing .& Nil)
           mask        <- prim S.slice_axis (#data := mask
                                          .& #axis  := 0
-                                         .& #begin := 0
-                                         .& #end := Just max_pos .& Nil)
+                                         .& #begin := max_pos
+                                         .& #end := Nothing .& Nil)
           ious_argmax <- prim S.slice_axis (#data := ious_argmax
                                          .& #axis  := 0
-                                         .& #begin := 0
-                                         .& #end := Just max_pos .& Nil)
+                                         .& #begin := max_pos
+                                         .& #end := Nothing .& Nil)
           -- class 2 ==> class 4
-          class_4 <- onesLike ious_max >>= mulScalar 4
-          cond  <- eqScalar 2 mask
-          mask  <- where_ cond class_4 mask
-          order <- prim S.argsort (#data := mask .& #is_ascend := False .& Nil)
+          class_4 <- onesLike mask >>= mulScalar 4
+          cond    <- eqScalar 2 mask
+          mask    <- where_ cond class_4 mask
+          order   <- prim S.argsort (#data := mask .& #is_ascend := False .& Nil)
 
           let num_neg = num_sample - max_pos
           bottomk <- prim S.slice_axis (#data := order
                                      .& #axis  := 0
                                      .& #begin := 0
                                      .& #end := Just num_neg .& Nil)
-          bottomk_indices <- takeI index       bottomk
-          bottomk_samples <- takeI mask        bottomk
-          bottomk_matches <- takeI ious_argmax bottomk
+          bottomk_indices <- takeI bottomk index
+          bottomk_samples <- takeI bottomk mask
+          bottomk_matches <- takeI bottomk ious_argmax
 
-          cond <- eqScalar 3 bottomk_samples
           -- class 4 ==> label -1
           -- class 3 ==> label 1
           -- class 0 ==> label 0
           cond <- eqScalar 3 bottomk_samples
+          pos_class <- onesLike bottomk_samples
           bottomk_samples <- where_ cond pos_class bottomk_samples
           cond <- eqScalar 4 bottomk_samples
+          neg_class <- onesLike bottomk_samples >>= mulScalar (-1)
           bottomk_samples <- where_ cond neg_class bottomk_samples
 
           -- concat
@@ -132,13 +141,13 @@ rcnnSampler batch_size num_proposal num_sample fg_overlap fg_fraction max_num_gt
           samples <- concat_ 0 [topk_samples, bottomk_samples]
           matches <- concat_ 0 [topk_matches, bottomk_matches]
 
-          sampled_rois <- takeI rois indices
+          sampled_rois <- takeI indices all_rois
           [x1, y1, x2, y2] <- splitBySections 4 (-1) True sampled_rois
           rois_areas <- join $ liftM2 mul_ (sub_ x2 x1) (sub_ y2 y1)
           ind <- prim S.argsort (#data := rois_areas .& Nil)
-          r <- takeI sampled_rois ind
-          s <- takeI samples      ind
-          m <- takeI matches      ind
+          r <- takeI ind sampled_rois
+          s <- takeI ind samples
+          m <- takeI ind matches
           return (r, s, m)
 
       getBatch s i = squeeze (Just [0]) =<<
@@ -156,9 +165,9 @@ rcnnTargetGenerator :: Int -> Int -> Int
                     -> SymbolHandle
                     -> SymbolHandle
                     -> SymbolHandle
-                    -> Layer (SymbolHandle, SymbolHandle, SymbolHandle)
+                    -> Layer (SymbolHandle, SymbolHandle, SymbolHandle, SymbolHandle)
 rcnnTargetGenerator batch_size num_fg_classes max_pos samples matches anchors gt_label gt_boxes mean std = do
-    -- B: batch_size, N: num_rois, M: num_gt, N_pos: num_fg_roi, C: num_fg_classes
+    -- B: batch_size, N: num_rois, M: num_gt, N_pos: max_pos, C: num_fg_classes
     --
     -- samples: (B, N), value -1 (negative), 0 (ignore), 1 (positive)
     -- matches: (B, N), value range [0, M), the best-matched gt of each roi
@@ -169,24 +178,26 @@ rcnnTargetGenerator batch_size num_fg_classes max_pos samples matches anchors gt
     -- std:  (4,)
     --
     -- returns:
-    --   targets: (B, N_pos, C, 4)
-    --   masks:   (B, N_pos, C, 4)
-    --   indices: (B, N_pos)
+    --   cls_targets: (B, N_pos), value [0, num_classes]
+    --   box_targets: (B, N_pos, C, 4)
+    --   box_masks:   (B, N_pos, C, 4)
+    --   mask_sel:    (B, N_pos)
     --
     labels <- reshape [0, 1, -1] gt_label
     labels <- prim S.broadcast_like (#lhs := labels .& #rhs := matches .& #lhs_axes := Just [1] .& #rhs_axes := Just [1] .& Nil)
     -- labels: (B,N,M) forall batch, roi, gt. class id
-    -- target_labels_fg: (B,N) forall batch, roi, class id of the best gt
-    target_labels_fg <- prim S.pick (#data := labels .& #index := matches .& #axis := Just 2 .& Nil)
+    -- fg_cls_targets: (B,N) forall batch, roi, class id of the best gt
+    fg_cls_targets <- prim S.pick (#data := labels .& #index := matches .& #axis := Just 2 .& Nil)
     -- shift by 1, reserve 0 for the background class
-    target_labels <- addScalar 1 target_labels_fg
+    cls_targets <- addScalar 1 fg_cls_targets
 
-    ign <- onesLike target_labels >>= mulScalar (-1)
-    bck <- zerosLike target_labels
+    ign <- onesLike cls_targets >>= mulScalar (-1)
+    bck <- zerosLike cls_targets
     pos <- gtScalar 0.5 samples
     neg <- ltScalar (-0.5) samples
-    target_labels <- where_ pos target_labels ign
-    target_labels <- where_ neg bck target_labels
+    -- [1, num_fg_classes] for fg, 0 for background, -1 for being ignored
+    cls_targets <- where_ pos cls_targets ign
+    cls_targets <- where_ neg bck cls_targets
 
     ret <- prim S._contrib_box_encode (#samples := samples
                                     .& #matches := matches
@@ -194,28 +205,28 @@ rcnnTargetGenerator batch_size num_fg_classes max_pos samples matches anchors gt
                                     .& #refs    := gt_boxes
                                     .& #means   := mean
                                     .& #stds    := std .& Nil)
-    [targets, masks] <- mapM (ret `at`) ([0, 1] :: [Int])
+    [box_targets, box_masks] <- mapM (ret `at`) ([0, 1] :: [Int])
 
-    target_labels_fg <- expandDims 2 target_labels_fg
+    fg_cls_targets <- expandDims 2 fg_cls_targets
     class_ids_fg <- prim S._arange (#start := 0 .& #stop := Just (fromIntegral num_fg_classes) .& Nil)
     class_ids_fg <- reshape [1,1,-1] class_ids_fg
-    -- (B, N, C), one hot indicator for the fg classes for each roi of each batch
-    target_class_fg_onehot <- eqBroadcast target_labels_fg class_ids_fg
+    -- (B, N, C), one hot indicator for the best gt class id for each roi of each batch
+    target_class_fg_onehot <- eqBroadcast fg_cls_targets class_ids_fg
 
-    masks_sel <- prim S.slice_axis (#data := masks .& #axis := -1 .& #begin := 0 .& #end := Just 1 .& Nil)
+    masks_sel <- prim S.slice_axis (#data := box_masks .& #axis := -1 .& #begin := 0 .& #end := Just 1 .& Nil)
     masks_sel <- prim S.argsort (#data := masks_sel .& #axis := Just 1 .& #is_ascend := False .& Nil)
     masks_sel <- reshape [batch_size, -1] masks_sel
     -- mask indices of those positive ones (take at most max_pos items)
     masks_sel <- prim S.slice_axis (#data := masks_sel .& #axis := 1 .& #begin := 0 .& #end := Just max_pos .& Nil)
 
-    (targets, masks, clsid_ohs) <- fmap unzip3 $ forM [0..batch_size-1] $ \i -> do
+    (box_targets, box_masks, clsid_ohs) <- fmap unzip3 $ forM [0..batch_size-1] $ \i -> do
         ind    <- prim S.slice_axis (#data := masks_sel .& #axis := 0 .& #begin := i .& #end := Just (i+1) .& Nil)
                     >>= squeeze (Just [0])
 
-        target <- prim S.slice_axis (#data := targets   .& #axis := 0 .& #begin := i .& #end := Just (i+1) .& Nil)
+        target <- prim S.slice_axis (#data := box_targets .& #axis := 0 .& #begin := i .& #end := Just (i+1) .& Nil)
                     >>= squeeze (Just [0])
 
-        mask   <- prim S.slice_axis (#data := masks     .& #axis := 0 .& #begin := i .& #end := Just (i+1) .& Nil)
+        mask   <- prim S.slice_axis (#data := box_masks   .& #axis := 0 .& #begin := i .& #end := Just (i+1) .& Nil)
                     >>= squeeze (Just [0])
 
         clsid_oh <- prim S.slice_axis (#data := target_class_fg_onehot .& #axis := 0 .& #begin := i .& #end := Just (i+1) .& Nil)
@@ -227,11 +238,13 @@ rcnnTargetGenerator batch_size num_fg_classes max_pos samples matches anchors gt
 
         return (target, mask, clsid_oh)
 
-    targets   <- concat_ 0 (targets  :: [SymbolHandle]) >>= expandDims 2
-    masks     <- concat_ 0 (masks    :: [SymbolHandle]) >>= expandDims 2
-    clsid_ohs <- concat_ 0 (clsid_ohs:: [SymbolHandle]) >>= expandDims 3 >>= broadcastAxis [3] [4]
+    box_targets <- concat_ 0 (box_targets :: [SymbolHandle]) >>= expandDims 2
+    box_masks   <- concat_ 0 (box_masks   :: [SymbolHandle]) >>= expandDims 2
+    -- broadcast the one-hot indicator
+    clsid_ohs   <- concat_ 0 (clsid_ohs:: [SymbolHandle]) >>= expandDims 3 >>= broadcastAxis [3] [4]
 
-    targets <- broadcastAxis [2] [num_fg_classes] targets
-    masks   <- mulBroadcast masks clsid_ohs
-    return (targets, masks, masks_sel)
+    box_targets <- broadcastAxis [2] [num_fg_classes] box_targets
+    box_masks   <- mulBroadcast box_masks clsid_ohs
+    -- return the index of positive masks because we will calculate box loss only on those items
+    return (cls_targets, box_targets, box_masks, masks_sel)
 
